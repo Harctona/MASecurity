4831eea7b5e332736cc40271a0c27613bb699a14
Harctona <harctona@gmail.com>
2016-08-30 21:26:50 +0100

    left fix

--- js/init.js
+++ js/init.js
@@ -4,7 +4,7 @@
     $('.collapsible').collapsible();
     $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
+      edge: 'left', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
   }); // end of document ready
