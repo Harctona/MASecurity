a2bd3896ca040003b1534a03f7b89b4aad16bd63
Harctona <harctona@gmail.com>
2016-08-30 21:23:21 +0100

    jkvfhjvb

--- js/init.js
+++ js/init.js
@@ -1,15 +1,11 @@
 (function($){
   $(function(){
 
-  // Initialize collapse button
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  $('.collapsible').collapsible();
+    //$('.collapsible').collapsible();
     $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
       edge: 'right', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
-  );
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
