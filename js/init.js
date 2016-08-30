7e7a2191cfda57e1c70794e2e7a177f08204c7f7
Harctona <harctona@gmail.com>
2016-08-30 21:25:58 +0100

    fix );

--- js/init.js
+++ js/init.js
@@ -6,6 +6,6 @@
       menuWidth: 300, // Default is 240
       edge: 'right', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
